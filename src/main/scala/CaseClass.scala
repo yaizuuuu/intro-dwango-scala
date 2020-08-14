3e478f5d2dd857dcbe658c3c838bf85a2be609ba
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-08-14 22:42:02 +0900

    ケースクラスとパターンマッチングの写経

--- src/main/scala/CaseClass.scala
+++ src/main/scala/CaseClass.scala
@@ -67,7 +67,7 @@ object CaseClass {
   // case classを簡単に記述すると以下のようになる
   class Point2(val x: Int, val y: Int) {
     override def equals(obj: Any): Boolean = obj match {
-      case thatPoint: Point =>
+      case thatPoint: Point2 =>
         thatPoint.canEqual(this) && this.x == thatPoint.x && this.y == thatPoint.y
       case _ =>
         false
@@ -75,7 +75,7 @@ object CaseClass {
 
     override def hashCode(): Int = x.hashCode() ^ y.hashCode()
 
-    def canEqual(that: Any): Boolean = that.isInstanceOf[Point]
+    def canEqual(that: Any): Boolean = that.isInstanceOf[Point2]
   }
 
   object Point2 {
@@ -97,7 +97,9 @@ object CaseClass {
   println(nextDayOfWeek(Sunday))
 
   sealed abstract class Tree
+
   case class Branch(value: Int, left: Tree, right: Tree) extends Tree
+
   case object Empty extends Tree
 
   val tree: Tree = Branch(1, Branch(2, Empty, Empty), Branch(3, Empty, Empty))
@@ -112,7 +114,7 @@ object CaseClass {
         else maxVal
       case Branch(v, Empty, r) =>
         val maxVal = max(r)
-        if(v > maxVal) v
+        if (v > maxVal) v
         else maxVal
       case Branch(v, l, r) =>
         val lmaxVal = max(l)
@@ -122,11 +124,51 @@ object CaseClass {
         if (lmaxVal > rmaxVal) maxVal = lmaxVal
         else maxVal = rmaxVal
 
-        if(v > maxVal) v
+        if (v > maxVal) v
         else maxVal
       case Empty => 0
     }
   }
 
   println(max(tree))
+
+  def depth(tree: Tree): Int = {
+    tree match {
+      case Branch(_, Empty, Empty) =>
+        1
+      case Branch(_, l, Empty) =>
+        1 + depth(l)
+      case Branch(_, Empty, r) =>
+        1 + depth(r)
+      case Branch(_, l, r) =>
+        val ld = depth(l)
+        val rd = depth(r)
+
+        var d: Int = 0
+        if (ld > rd) d = ld
+        else d = rd
+
+        1 + d
+      case Empty =>
+        0
+    }
+  }
+
+  println(depth(tree))
+
+  val even: Int => Boolean = {
+    case i if i % 2 == 0 => true
+    case _ => false
+  }
+
+  println(even(5))
+
+  val even2: Int => Boolean = (x => x match {
+    case i if i % 2 == 0 => true
+    case _ => false
+  })
+
+  println(even2(3))
+
+  println(List(1, 2, 3, 4, 5).collect { case i if i % 2 == 1 => i * 2 })
 }
