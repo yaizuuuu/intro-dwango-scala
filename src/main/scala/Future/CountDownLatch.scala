a830f04efdba65c073784c0250c7e8c05a08b606
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-08-23 23:06:20 +0900

    FutureとPromiseの課題を実装

--- src/main/scala/Future/CountDownLatch.scala
+++ src/main/scala/Future/CountDownLatch.scala
@@ -1,5 +1,28 @@
 package Future
 
-object CountDownLatch {
+import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Promise, Future}
+import scala.util.Random
 
+object CountDownLatch extends App {
+  val indexHolder = new AtomicInteger(0)
+  val random = new Random()
+  val promises: Seq[Promise[Int]] = for {i <- 1 to 3} yield Promise[Int]
+  val futures: Seq[Future[Int]] = for {i <- 1 to 8} yield Future[Int] {
+    val waitMilliSec = random.nextInt(1001)
+    Thread.sleep(waitMilliSec)
+    waitMilliSec
+  }
+
+  futures.foreach { f => f.foreach { case waitMilliSec =>
+    val index = indexHolder.getAndIncrement
+
+    if (index < promises.length) {
+      promises(index).success(waitMilliSec)
+    }
+  }}
+
+  promises.foreach( p => p.future.foreach { case waitMilliSec => println(waitMilliSec) })
+  Thread.sleep(5000)
 }
